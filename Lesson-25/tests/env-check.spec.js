738f8e3fab89d8c68f50014ae03207147c9019a6
Ketrinzera <125549014+Ketrinzera@users.noreply.github.com>
2025-05-19 00:14:47 +0300

    Delete Lesson-25 directory

--- Lesson-25/tests/env-check.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example test', async ({ page }) => {
-  await page.goto(process.env.BASE_URL);
-  expect(await page.title()).not.toBeNull();
-});
-
